b1bb16f80ac2bb0a0a04d5535979e9a49c7e9d9e
Dinyo Ilchev Dinev <dinio.dinev@gmail.com>
2018-11-15 23:11:43 +0200

    Add events

--- ng5/src/app/home/home.component.ts
+++ ng5/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  itemCount: number;
+  btnText: string = 'Add item';
+  goalText: string = 'My fist life goal.';
+  goals = [];
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem() {
+  this.goals.push(this.goalText);
+  this.goalText = '';
+  this.itemCount = this.goals.length;
   }
 
 }
